e7be9135afdf87906168010bba6213d681b8abe3
robsonjr8 <robson.marques.junior@protonmail.com>
2025-03-22 22:55:49 +0000

    Creating environment variable and fixing out warnings

--- main.py
+++ main.py
@@ -1,28 +1,25 @@
 import asyncio
 import json
+import os
 
-from db_driver import execute
+from dotenv import load_dotenv
+
+from db_driver.db_driver import execute
 
 
 def main():
-    loop = asyncio.get_event_loop()  # TODO: DeprecationWarning: There is no current event loop
-    loop.run_until_complete(create_tasks_func())
-    loop.close()
+    asyncio.run(fetch())
 
 
 async def fetch():
-    data = await execute("mysql://user:password@host:port/db", 18)
+    load_dotenv()
+    data = await execute(os.environ.get("DB_URI", "db-uri-undefined"), 18)
     print(data)
     print(type(data))
     json_data = json.loads(data.decode("utf-8"))
     print(json_data)
     print(type(json_data))
     print(type(json_data[0]))
-
-
-async def create_tasks_func():
-    tasks = [asyncio.create_task(fetch())]
-    await asyncio.wait(tasks)
 
 
 if __name__ == "__main__":
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use mysql_async::{params};
+use mysql_async::{params, Pool};
 use mysql_async::prelude::Queryable;
 use pyo3::{prelude::*, wrap_pyfunction};
 use serde::Serialize;
@@ -6,9 +6,9 @@ use serde::Serialize;
 #[pyclass(extends=pyo3::exceptions::PyException)]
 #[derive(Debug, PartialEq, Eq, Serialize)]
 struct Auditdata {
-    id: i32,
+    id: u32,
     name: String,
-    age: i32,
+    age: u8,
 }
 
 impl<'py> IntoPyObject<'py> for Auditdata {
@@ -24,19 +24,39 @@ impl<'py> IntoPyObject<'py> for Auditdata {
 #[pymethods]
 impl Auditdata {
     #[new]
-    fn new(id: i32, age: i32, name: String) -> Auditdata {
+    fn new(id: u32, age: u8, name: String) -> Auditdata {
         Auditdata { id, name, age }
     }
 }
 
+// #[pyfunction]
+// fn execute(py: Python, uri: String, age: i8) -> PyResult<Bound<PyAny>> {
+//     pyo3_async_runtimes::tokio::future_into_py(py, async move {
+//         let opts = mysql_async::Opts::from_url(uri.as_str()).unwrap();
+//         let pool = Pool::new(opts);
+//         let mut conn = pool.get_conn().await.expect("mysql_async::Pool::get_conn");
+//         let data: Vec<_> = conn.exec_map(
+//             r"SELECT id, name, age
+//                   FROM person
+//                   WHERE age >= :age",
+//             params! { "age" => age },
+//             |(id, name, age)| {Auditdata { id, name, age }},
+//         ).await.expect("mysql_async::Params::get");
+//         drop(conn);
+//         pool.disconnect().await.expect("mysql_async::Pool::disconnect");
+//         let json_result = serde_json::to_vec_pretty(&data).unwrap();
+//         Ok(json_result)
+//     })
+// }
+
 #[pyfunction]
-fn execute(py: Python, uri: String, age: i8) -> PyResult<Bound<PyAny>> {
+fn execute(py: Python, uri: String, age: u8) -> PyResult<Bound<PyAny>> {
     pyo3_async_runtimes::tokio::future_into_py(py, async move {
         let opts = mysql_async::Opts::from_url(uri.as_str()).unwrap();
-        let pool = mysql_async::Pool::new(opts);
+        let pool = Pool::new(opts);
         let mut conn = pool.get_conn().await.expect("mysql_async::Pool::get_conn");
         let data: Vec<_> = conn.exec_map(
-            r"SELECT id, name, age
+            "SELECT id, name, age
                   FROM person
                   WHERE age >= :age",
             params! { "age" => age },
@@ -49,19 +69,83 @@ fn execute(py: Python, uri: String, age: i8) -> PyResult<Bound<PyAny>> {
     })
 }
 
-// #[pyclass]
 // struct Session {
-//     uri: String,
+//     conn: Conn,
+//     pool: Pool,
 // }
 //
+// impl<'py> IntoPyObject<'py> for Session {
+//     type Target = PyAny;
+//     type Output = Bound<'py, Self::Target>;
+//     type Error = PyErr;
+//
+//     fn into_pyobject(self, py: Python<'py>) -> Result<Self::Output, Self::Error> {
+//         py.get_type::<Session>().call1((self.conn, self.pool))
+//     }
+// }
+//
+// // #[pymethods]
+// // impl Session {
+// //     #[new]
+// //     fn new(conn: Conn, pool: Pool) -> Session {
+// //         Session { conn, pool }
+// //     }
+// // }
+//
 // trait DbSession {
-//     fn connect(uri: &str) -> Self;
-//     fn disconnect(&self);
-//     fn execute(stmt: &str, params: Vec<T>) -> Self;
+//     fn new(conn: Conn, pool: Pool) -> Self;
+//     async fn execute(self, stmt: &str, age: i32) -> Result<Vec<u8>, ()>;
+// }
+//
+// impl DbSession for Session {
+//     fn new(conn: Conn, pool: Pool) -> Session {
+//         Session {conn, pool}
+//     }
+//
+//     async fn execute(mut self, stmt: &str, age: i32) -> Result<Vec<u8>, ()> {
+//         let data: Vec<Auditdata> = self.conn.exec_map(
+//             stmt,
+//             params! { "age" => age },
+//             |(id, name, age)| {Auditdata { id, name, age }}
+//         ).await.expect("mysql_async::Params::get");
+//         drop(self.conn);
+//         self.pool.disconnect().await.expect("mysql_async::Pool::disconnect");
+//         let json_result = serde_json::to_vec_pretty(&data).unwrap();
+//         Ok(json_result)
+//     }
+// }
+//
+// // async fn query(uri: String, age: i8) -> Result<Vec<u8>, ()> {
+// //     let opts = mysql_async::Opts::from_url(uri.as_str()).unwrap();
+// //     let pool = mysql_async::Pool::new(opts);
+// //     let mut conn = pool.get_conn().await.expect("mysql_async::Pool::get_conn");
+// //     let data: Vec<_> = conn.exec_map(
+// //         r"SELECT id, name, age
+// //               FROM person
+// //               WHERE age >= :age",
+// //         params! { "age" => age },
+// //         |(id, name, age)| {Auditdata { id, name, age }},
+// //     ).await.expect("mysql_async::Params::get");
+// //     drop(conn);
+// //     pool.disconnect().await.expect("mysql_async::Pool::disconnect");
+// //     let json_result = serde_json::to_vec_pretty(&data).unwrap();
+// //     Ok(json_result)
+// // }
+//
+// #[pyfunction]
+// fn session(py: Python, uri: String) -> PyResult<Bound<PyAny>> {
+//     pyo3_async_runtimes::tokio::future_into_py(py, async move {
+//         let opts = mysql_async::Opts::from_url(uri.as_str()).unwrap();
+//         let pool = Pool::new(opts);
+//         let conn = pool.get_conn().await.expect("mysql_async::Pool::get_conn");
+//         let db: Session = DbSession::new(conn, pool);
+//         Ok(db)
+//     })
 // }
 
 #[pymodule]
 fn db_driver(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(execute, m)?)?;
+    // m.add_function(wrap_pyfunction!(session, m)?)?;
     Ok(())
 }
